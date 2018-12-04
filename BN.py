d1fb4e711633577311243d1f6a8464eb85e7b211
Diogo Redin <diogo.redin@gmail.com>
2018-12-04 17:14:25 +0000

    New data structure

--- BN.py
+++ BN.py
@@ -3,75 +3,40 @@
 import copy
 
 class Factor():
-	def __init__(self, prob, unit_list, from_node):
+	def __init__(self, prob, unit_list):
 		prob_list = [0]*(2**len(unit_list))
-		print(prob_list)
-		if len(prob) == 1:
-			self.prob = [1 - prob[0], prob[0]]
+		if len(prob_list) == 2:
+			prob_list[1] = prob[0]
+			prob_list[0] = 1 - prob[0]
 		else:
-			self.prob = prob
-		
-			if from_node:
-				list_negative = copy.deepcopy(self.prob)
+			for i in range(2**(len(unit_list)-1)):
+				aux_prob = prob
+				idx = []
+				val = 0
+				for binary in range(len(unit_list) - 1):
+					idx.insert(0,int(i & 1 << binary != 0))
+				for index in idx:
+					aux_prob = aux_prob[index]
+				print (aux_prob)
+				putOnList(prob_list, idx, aux_prob)
+			
+		print(prob_list)
 
-				for i in range(2**(len(unit_list)-1)):
-					l = list_negative
-					idx = makeIdxCombo(len(unit_list)-1, i)
+def getFromList(l, index):
+	actual_index = 0
+	for i in range(len(index)):
+		if index[i] != 0:
+			actual_index += 1 << i
+	return l[actual_index]
 
-					for index in idx[:-1]:
-						l = l[int(index)]
-					l[idx[-1]] = 1 - l[idx[-1]]
 
-				self.prob = [list_negative, self.prob]
-		self.unit_list = unit_list
 
-	def getProb(self, evid):
-		p = self.prob
-		for val in evid[1:]:
-			p = p[val]
-		return p
-
-	def sumOut(self, unit):
-		evid = []
-		for i in range(len(self.unit_list)):
-			evid.append(0)
-			if unit == self.unit_list[i]:
-				unit_pos = i
-				break
-
-		new_prob = []
-		for i in range(2**(len(self.unit_list))):
-
-			idx = makeIdxCombo(len(self.unit_list), i)
-			new_idx = idx.copy()
-			new_idx.pop(unit_pos)
-			iterator = new_prob
-			for index in new_idx:
-				
-				if len(iterator) == 0:
-					iterator.extend([[],[]])
-
-			val = self.sumProbPair(idx,unit_pos)
-			for index in new_idx[:-1]:
-				iterator = iterator[index]
-			iterator.append(val)
-		
-		return iterator
-
-	def sumProbPair(self, idx, unit_pos):
-		prob = self.prob
-		val1 = 0
-		val2 = 0
-		idx[unit_pos] = 0
-		for index in idx:
-			prob = prob[index]
-		val1 = prob
-		idx[unit_pos] = 1
-		prob = self.prob
-		for index in idx:
-			prob = prob[index]
-		val2 = prob
-		return val1 + val2
+def putOnList(l, index, val):
+	actual_index = 0
+	for i in range(len(index)):
+		if index[i] != 0:
+			actual_index += 1 << i
+	l[actual_index] = val
 
 class Node():
 	def __init__(self, prob, parents = []):
