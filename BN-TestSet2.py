0688dc633144318110d5eb0a56c4b236bb752e78
JoaoMiguelNeves <jmcpneves@gmail.com>
2018-12-04 20:26:29 +0000

    changed node format, some test changes

--- BN-TestSet2.py
+++ BN-TestSet2.py
@@ -12,14 +12,12 @@ print( "p1 false %.4e p1 true %.4e" % (p1.computeProb(ev)[0] , p1.computeProb(ev
 p2 = Node( np.array([.5,.1]), gra[1] )# sprinkler
 
 p3 = Node( np.array([.2,.8]), gra[2] )# rain
-
 p4 = Node( np.array([[.0,.9],[.9,.99]]), gra[3] )# wetgrass
-print( "p2 = 1, p3 = 1, p4 false %.4e p4 true %.4e" % (p4.computeProb(ev)[0] , p4.computeProb(ev)[1])) 
+print( "p2 = 1, p3 = 1, p4 false %.4e p4 true %.4e" % (p4.computeProb(ev)[0] , p4.computeProb(ev)[1]))
 
 prob = [p1,p2,p3,p4]
 
 bn = BN(gra, prob)
-
 jp = []
 for e1 in [0,1]:
 	for e2 in [0,1]:
@@ -33,11 +31,8 @@ print("sum joint %.3f (1)" % sum(jp))
 ### Tests to joint Prob
 ev = (0,0,0,0)
 print( "joint %.4g (0.2)" % bn.computeJointProb(ev) ) 
-
 ev = (1,1,1,1)
-print( "joint %.4g (0.0396)" % bn.computeJointProb(ev) ) 
-
-'''
+print( "joint %.4g (0.0396)" % bn.computeJointProb(ev) )
 ### Tests to post Prob
 # P(e1|e4=1)
 ev = (-1,[],[],1)
@@ -50,9 +45,9 @@ ev = (1,[],[],-1)
 print("ev : ")
 print(ev)
 print( "post : %.4g (0.7452)" % bn.computePostProb(ev)  )
-'''
 # P(e1|e2=0,e3=0)
 ev = (-1,0,0,[])
 print("ev : ")
 print(ev)
 print( "post : %.4g (0.3103)" % bn.computePostProb(ev)  )
+
--- BN.py
+++ BN.py
@@ -1,100 +1,57 @@
 # -*- coding: utf-8 -*-
 
-import copy
+def flatten(l):
 
-class Factor():
-	def __init__(self, prob, unit_list):
-		prob_list = [0]*(2**len(unit_list))
-		if len(prob_list) == 2:
-			prob_list[1] = prob[0]
-			prob_list[0] = 1 - prob[0]
-		else:
-			for i in range(2**(len(unit_list)-1)):
-				aux_prob = prob
-				idx = []
-				val = 0
-				for binary in range(len(unit_list) - 1):
-					idx.insert(0,int(i & 1 << binary != 0))
-				for index in idx:
-					aux_prob = aux_prob[index]
-				print (aux_prob)
-				putOnList(prob_list, idx, aux_prob)
-			
-		print(prob_list)
+	'''
+	Flattens a list with an arbitrary number of dimensions, creating a single, one dimensional list.
+	O(n * m) -> n is the list size, m is the max list depth
+	'''
+	i = 0
+	while i < len(l):
+		while isinstance(l[i], list):
+			if l[i] == []:
+				del l[i]
+				i -= 1
+				break
+			else:
+				l[i:i + 1] = l[i]
+		i += 1
+	return l
 
-def getFromList(l, index):
-	actual_index = 0
-	for i in range(len(index)):
-		if index[i] != 0:
-			actual_index += 1 << i
-	return l[actual_index]
-
-
-
-def putOnList(l, index, val):
-	actual_index = 0
-	for i in range(len(index)):
-		if index[i] != 0:
-			actual_index += 1 << i
-	l[actual_index] = val
 
 class Node():
 	def __init__(self, prob, parents = []):
-		self.prob = prob
+		prob = prob.tolist()
+		self.prob = flatten(prob)
+		
 		self.parents = parents
 
 	def computeProb(self, evid):
-
+		
 		# If there are no parents, the probability is trivial
 		if self.parents == []:
 			p = self.prob[0]
 
-		# If there are parents, evaluate each parent
+		# If there are parents, evaluate each parent	
 		else:
-			p = self.prob
+			index = 0
+			dad_count = len(self.parents) - 1
 			for dad in self.parents:
-				p = p[evid[dad]]
-
-		# Returns a tuple corresponding to: (P(self = false), P(self = true))
+				index += (1 & evid[dad]) << dad_count
+				dad_count -= 1
+			p = self.prob[index]
 		return (1 - p, p)
-	
+
 class BN():
 	def __init__(self, gra, nodes):
 		self.gra = gra
 		self.nodes = nodes
 
 	def computePostProb(self, evid):
-
-		# Create a list of factors corresponding to : [index,parents,...]
-		factors = []
-		for i in range(len(self.nodes)):
-			unit_list = []
-			unit_list.append(i)
-			for dad in self.nodes[i].parents:
-				unit_list.append(dad)
-			factors.append(Factor(self.nodes[i].prob, unit_list, True))
-
-		return 0
-
-		ign = []
-		for i in range(len(evid), 0, -1):
-			if evid[i] == []:
-				ign.append(i)
-			elif evid[i] == -1:
-				query = i
-	
 		return 0
 	
 	def computeJointProb(self, evid):
 		res = 1
-		i = 0
-		for prob in self.nodes:
-			res *= prob.computeProb(evid)[evid[i]]
-			i += 1
+		for i in range(len(self.nodes)):
+			res *= self.nodes[i].computeProb(evid)[evid[i]]
 		return res
-
-def makeIdxCombo(size, i):
-	idx = []
-	for binary in range(size):
-		idx.append(int(i & 1 << binary != 0))
-	return idx
